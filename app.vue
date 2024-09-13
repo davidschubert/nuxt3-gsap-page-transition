171a4f20457350d34d3108c819d9979e540cc46a
David Schubert <github@davidschubert.com>
2024-09-13 13:40:04 -1000

    update of gsap.client.ts

--- app.vue
+++ app.vue
@@ -9,36 +9,43 @@
     </div>
 </template>
 
+<script setup lang="ts">
+    import { gsap } from "gsap";
+    import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+    gsap.registerPlugin(ScrollTrigger);
+</script>
+
 <style>
-.page-enter-active,
-.page-leave-active {
-    transition: all 1.4s;
-}
-.page-enter-from,
-.page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-}
+    .page-enter-active,
+    .page-leave-active {
+        transition: all 1.4s;
+    }
+    .page-enter-from,
+    .page-leave-to {
+        opacity: 0;
+        filter: blur(1rem);
+    }
 
-.layout-enter-active,
-.layout-leave-active {
-    transition: all 1.4s;
-}
-.layout-enter-from,
-.layout-leave-to {
-    transform: translateY(80px);
-}
+    .layout-enter-active,
+    .layout-leave-active {
+        transition: all 1.4s;
+    }
+    .layout-enter-from,
+    .layout-leave-to {
+        transform: translateY(80px);
+    }
 
-.rotate-enter-active,
-.rotate-leave-active {
-    transition: all 1.4s;
-}
-.rotate-enter-from {
-    opacity: 0;
-    transform: translateY(80px);
-    transition-timing-function: cubic-bezier(0.42, 0, 0.58, 1);
-}
-.rotate-leave-to {
-    opacity: 0;
-}
+    .rotate-enter-active,
+    .rotate-leave-active {
+        transition: all 1.4s;
+    }
+    .rotate-enter-from {
+        opacity: 0;
+        transform: translateY(80px);
+        transition-timing-function: cubic-bezier(0.42, 0, 0.58, 1);
+    }
+    .rotate-leave-to {
+        opacity: 0;
+    }
 </style>
